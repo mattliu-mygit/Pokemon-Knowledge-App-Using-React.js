51a0f484038931c9cec708dbe4219adfcc543466
Matthew Liu <mliu78@jh.edu>
2020-06-17 18:35:22 -0400

    Build removed, minor instructions update
    1. Learned a bit about deploying server-rendered reactful apps, and will look into using nginx to deploy it sometime later maybe.

--- src/components/StartScreen.js
+++ src/components/StartScreen.js
@@ -3,7 +3,7 @@ import React from 'react';
 // The start screen that the user first sees when opening the app.
 const StartScreen = (props) => {
     return <div>
-        <div className = 'description'>A Pokemon knowledge quiz game made with React.js and the PokeAPI, the Restful Pokemon API, developed by Matthew Liu and Kevin Liu.</div>
+        <div className = 'description'>A Pokemon knowledge quiz game made with React.js and the PokeAPI, the Restful Pokemon API, developed by Matthew Liu and (future hopefully) css by Kevin Liu.</div>
         <div className = 'instructions'>
             <div>Test your Pokemon knowledge! In 60 seconds, try to match as many Pokemon to the characteristic thrown atcha! If your characteristic is too hard, feel free to reroll a new characteristic to test! Don't worry, you keep your score, even after rerolling! Be aware of the 5 second reroll timer huehuehue!</div>
             <div>Once you finish your game, have some fun giving the API some attributes to match in the results page!</div>
